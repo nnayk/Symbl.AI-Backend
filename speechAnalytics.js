c9fc17fa7b262e1f093de504d18bc7141c5edb50
nnayk <79224511+nnayk@users.noreply.github.com>
2022-08-26 23:28:08 -0700

    speech analytics done

--- speechAnalytics.js
+++ speechAnalytics.js
@@ -25,7 +25,7 @@ for (const val of rawSummary)
   tData.push(val['text'])
 }
 
-console.log(tData.join('\n'))
+// console.log(tData.join('\n'))
 
 return tData
 
@@ -33,7 +33,7 @@ return tData
 
 
 
-async function summary()
+async function getSummary()
 {
 let sumData = []
 const fetchResponse = await fetch(`https://api.symbl.ai/v1/conversations/${conversationId}/summary`, {
@@ -43,6 +43,7 @@ const fetchResponse = await fetch(`https://api.symbl.ai/v1/conversations/${conve
     'Content-Type': 'application/json'
   }
 });
+const responseBody = await fetchResponse.json();
 
 const rawSummary = responseBody['summary']
 for (const val of rawSummary)
@@ -50,12 +51,12 @@ for (const val of rawSummary)
   sumData.push(val['text'])
 }
 
-console.log(sumData.join('\n'))
+// console.log(sumData.join('\n'))
 
 return sumData
 }
 
-async function topics()
+async function getTopics()
 {
   let tData=[]
   const fetchResponse = await fetch(`https://api.symbl.ai/v1/conversations/${conversationId}/topics?sentiment=true&parentRefs=true`, {
@@ -66,18 +67,20 @@ async function topics()
   }
 });
 
+const responseBody = await fetchResponse.json();
+
 const rawSummary = responseBody['topics']
 for (const val of rawSummary)
 {
   tData.push(val['text'])
 }
 
-console.log(tData.join('\n'))
+// console.log(tData.join('\n'))
 
 return tData
 }
 
-async function questions()
+async function getQuestions()
 {
   let qData=[]
   const fetchResponse = await fetch(`https://api.symbl.ai/v1/conversations/${conversationId}/questions`, {
@@ -91,7 +94,7 @@ async function questions()
 const responseBody = await fetchResponse.json();
 
 // const rawData = JSON.stringify(responseBody2, null, 2);
-console.log(responseBody)
+// console.log(responseBody)
 // console.log("YELAY")
 // console.log(typeof responseBody2)
 // console.log(responseBody2['summary']['text'])
@@ -101,12 +104,12 @@ for (const val of rawSummary)
   qData.push(val['text'])
 }
 
-console.log(qData.join('\n'))
+// console.log(qData.join('\n'))
 
 return qData
 }
 
-async function analytics()
+async function getAnalytics()
 {
   let aData = []
   const fetchResponse = await fetch(`https://api.symbl.ai/v1/conversations/${conversationId}/analytics`, {
@@ -121,9 +124,9 @@ const responseBody = await fetchResponse.json();
 
 return responseBody
 }
-}
 
-async function actionItems()
+
+async function getActionItems()
 {
   let atData=[]
   const fetchResponse = await fetch(`https://api.symbl.ai/v1/conversations/${conversationId}/action-items`, {
@@ -136,7 +139,7 @@ async function actionItems()
 
 const responseBody = await fetchResponse.json();
 
-console.log(responseBody)
+// console.log(responseBody)
 
 const rawSummary = responseBody['actionItems']
 for (const val of rawSummary)
@@ -144,36 +147,99 @@ for (const val of rawSummary)
   atData.push(val['text'])
 }
 
-console.log(atData.join('\n'))
+// console.log(atData.join('\n'))
 
 return atData
 }
 
 async function getWPM()
 {
-  let metrics = await analytics()
+  let metrics = await getAnalytics()
   return metrics['members'][0]['pace']['wpm']
 }
 
 async function getTalkTime()
 {
-  let metrics = await analytics()
+  let metrics = await getAnalytics()
   return metrics['metrics'][1]['seconds'] 
 }
 
 async function getSilenceTime()
 {
-  let metrics = await analytics()
+  let metrics = await getAnalytics()
   return metrics['metrics'][0]['seconds'] 
 }
 
-// transcript()
-// summary()
-// topics()
-// questions()
-// analytics()
-// actionItems()
-getWPM().then((val)=>console.log(val))
-getTalkTime().then((val)=>console.log(val))
-getSilenceTime().then((val)=>console.log(val))
+async function getFillerWords()
+{
+  let script = await getTranscript()
+  script=script.join(' ').split(' ').map(v => v.toLowerCase())
+  let numFiller = 0
+  let numTotal = script.length
+  let fillerWords = ["um","uh","er","like","but","you know","hmm"]
+
+  for (const val of script)
+  {
+    if (fillerWords.includes(val)) 
+    {
+      // console.log(`NUMBER ${numFiller+1}: ${val}`)
+      numFiller++
+    }
+  }
+  return [numFiller,numTotal]
+}
+
+
+getTranscript().then((val)=>
+{
+  if (val.length>0) console.log("TRANSCRIPT:",val.join('\n'),"\n\n\n")
+  else console.log("EMPTY TRANSCRIPT")
+});
+
+getSummary().then((val)=>
+{
+  if (val.length>0) console.log("SUMMARY:",val.join('\n'),"\n\n\n")
+  else console.log("EMPTY SUMMARY")
+});
+
+getTopics().then((val)=>
+{
+  if (val.length>0) console.log("TOPICS:",val.join('\n'),"\n\n\n")
+  else console.log("EMPTY TOPICS")
+});
+
+getQuestions().then((val)=>
+{
+  if (val.length>0) console.log("QUESTIONS:",val.join('\n'),"\n\n\n")
+  else console.log("EMPTY QUESTIONS")
+});
+
+// getAnalytics().then((val)=>
+// {
+//   if (val.length>0) console.log("ANALYTICS:",val.join('\n'),"\n\n\n")
+//   else console.log("EMPTY ANALYTICS")
+// });
+
+getActionItems().then((val)=>
+{
+  if (val.length>0) console.log("ACTION ITEMS:",val.join('\n'),"\n\n\n")
+  else console.log("EMPTY ACTION ITEMS")
+});
+
+getWPM().then((val)=>
+{
+  console.log("WPM:",val,"\n\n\n")
+});
+
+getTalkTime().then((val)=>
+{
+  console.log("TALK TIME (SEC):",val,"\n\n\n")
+});
+
+getSilenceTime().then((val)=>
+{
+  console.log("SILENCE TIME (SEC):",val,"\n\n\n")
+});
+
+getFillerWords().then((val)=>console.log("NUMBER OF FILLER WORDS:",val[0],"out of",val[1],`(${((val[0]/val[1])*100).toFixed(2)}%)`))
 
